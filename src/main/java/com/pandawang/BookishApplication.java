7c9192d35a24dfd1ef14bf63e384a3f5395a1cd7
Xiaofeng Wang <xfun68@gmail.com>
2016-06-26 18:43:28 +0800

    add hello world spring mvc example

--- src/main/java/com/pandawang/BookishApplication.java
+++ src/main/java/com/pandawang/BookishApplication.java
@@ -1,10 +1,18 @@
 package com.pandawang;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class BookishApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookishApplication.class, args);
