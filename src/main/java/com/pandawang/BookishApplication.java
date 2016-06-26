4cd9f478e6dd5dfb5dbb8f3d41983ef96f4e8917
Xiaofeng Wang <xfun68@gmail.com>
2016-06-26 18:35:52 +0800

    add spring boot app created from start.spring.io by default

---
+++ src/main/java/com/pandawang/BookishApplication.java
@@ -0,0 +1,12 @@
+package com.pandawang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookishApplication.class, args);
+	}
+}
